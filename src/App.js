09ba8260f85ff434aa4a7222d73ecfa4e0311aef
seyityahya <seyityahya05@gmail.com>
2023-03-22 01:34:56 +0300

    satın al işlemi yapıldı

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import products from "./products.json";
+import Product from "./components/Product";
 
 function App() {
-  return <div>deneme</div>;
+  const [money] = useState(100);
+  const [basket, setBasket] = useState([]);
+
+  useEffect(() => {
+    console.log(basket);
+  }, [basket]);
+
+  return (
+    <>
+      <Header money={money} />
+      {products.map((product) => (
+        <Product
+          key={product.id}
+          basket={basket}
+          setBasket={setBasket}
+          product={product}
+        />
+      ))}
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Product.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+function Product({ product, basket, setBasket }) {
+  const basketItem = basket.find((item) => item.id === product.id);
+
+  const addBasket = () => {
+    const checkBasket = basket.find((item) => item.id === product.id);
+
+    if (checkBasket) {
+      checkBasket.amount += 1;
+      setBasket([
+        ...basket.filter((item) => item.id !== product.id),
+        checkBasket,
+      ]);
+    } else {
+      setBasket([
+        ...basket,
+        {
+          id: product.id,
+          amount: 1,
+        },
+      ]);
+    }
+  };
+  const removeBasket = () => {
+    const currentBasket = basket.find((item) => item.id === product.id);
+    const basketWithoutCurrent = basket.filter(
+      (item) => item.id !== product.id
+    );
+    currentBasket.amount -= 1;
+    if (currentBasket.amount === 0) {
+      setBasket([...basketWithoutCurrent]);
+    } else {
+      setBasket([...basketWithoutCurrent, currentBasket]);
+    }
+  };
+  return (
+    <>
+      <div className="product">
+        <h3>{product.title}</h3>
+        <div className="price">{product.price} $</div>
+        <div>
+          <button onClick={removeBasket}>Sat</button>
+          <span className="amount">
+            {(basketItem && basketItem.amount) || 0}
+          </span>
+          <button onClick={addBasket}>Satın Al</button>
+        </div>
+        <style jsx>{`
+          .product {
+            padding: 10px;
+            background: #fff;
+            border: 1px solid #ddd;
+            margin-bottom: 20px;
+          }
+        `}</style>
+      </div>
+    </>
+  );
+}
+
+export default Product;
